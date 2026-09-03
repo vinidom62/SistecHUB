3ebce654747cc64adbdb0f8709a04972fcdc6240
Vinicius Domingues Goveia <vinicius.goveia@sisteconsultoria.com.br>
2026-09-03 15:41:39 -0300

    2.1.6

--- Core/AppVersion.cs
+++ Core/AppVersion.cs
@@ -6,5 +6,5 @@ namespace SistecHub.Core;
 public static class AppVersion
 {
     /// <summary>Versão atual em semver (ex.: "1.1.0"). Deve coincidir com Version no .csproj e com vpk pack --packVersion.</summary>
-    public const string Current = "2.1.5";
+    public const string Current = "2.1.6";
 }
